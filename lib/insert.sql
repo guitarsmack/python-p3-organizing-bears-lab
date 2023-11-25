5b74fe56359f2ffd068429608283f2636a0fb180
Michael Alan Stowe <mastowe94@gmail.com>
2023-11-24 23:30:15 -0600

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 22, "M", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 22, "M", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "White", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 22, "M", "Green", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "Black", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, "F", "Black", "Friendly", 1);
